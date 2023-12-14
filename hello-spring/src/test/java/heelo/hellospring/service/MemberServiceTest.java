fff536de18d84786017005f59816304145b2e5b9
Kite_Z <duswl339@ajou.ac.kr>
2023-12-14 22:41:22 +0900

    [Section3] 회원 서비스 테스트

---
+++ hello-spring/src/test/java/heelo/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,57 @@
+package heelo.hellospring.service;
+
+import heelo.hellospring.domain.Member;
+import heelo.hellospring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
+
+    @BeforeEach
+    public void beforeEach() {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
+
+    @AfterEach
+    public void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    public void 회원가입() throws Exception {
+        // given
+        Member member = new Member();
+        member.setName("hello");
+
+        // when
+        Long saveId = memberService.join(member);
+
+        // then
+        Member findMember = memberRepository.findById(saveId).get();
+        Assertions.assertEquals(findMember.getName(), member.getName());
+    }
+
+    @Test
+    public void 중복_회원_예외() throws Exception {
+        // given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        // when
+        memberService.join(member1);
+        IllegalArgumentException e = Assertions.assertThrows(IllegalArgumentException.class, () ->
+                memberService.join(member2)); // 예외가 발생해야 한다.
+
+        Assertions.assertEquals(e.getMessage(), "이미 존재하는 회원입니다.");
+    }
+}

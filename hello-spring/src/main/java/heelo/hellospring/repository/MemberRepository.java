cbd9b8291d896d2bd664216dde2e720971fb0792
Kite_Z <duswl339@ajou.ac.kr>
2023-12-14 21:57:42 +0900

    Init

---
+++ hello-spring/src/main/java/heelo/hellospring/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package heelo.hellospring.repository;
+
+import heelo.hellospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}

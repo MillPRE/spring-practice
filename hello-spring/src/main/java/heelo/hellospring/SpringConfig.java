31b502752f22a04aad734d7718ade06f69695f22
Kite_Z <duswl339@ajou.ac.kr>
2023-12-19 17:46:32 +0900

    [Section6] 순수 JDBC

--- hello-spring/src/main/java/heelo/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/heelo/hellospring/SpringConfig.java
@@ -1,13 +1,23 @@
 package heelo.hellospring;
 
+import heelo.hellospring.repository.JdbcMemberRepository;
 import heelo.hellospring.repository.MemberRepository;
 import heelo.hellospring.repository.MemoryMemberRepository;
 import heelo.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    private final DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -17,6 +27,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
